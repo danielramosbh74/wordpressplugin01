64a68c9c56f5aa27f5f9527ec08191e1bf329741
Daniel Ramos de Carvalho <danielramosbh74@gmail.com>
2019-07-31 19:21:07 -0300

    First version of the most simple plugin

---
+++ plugin-webmaster01.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+	Plugin name: Plugin Webmaster 01
+	Plugin uri: https://www.webmaster.inf.br/downloads/plugins/
+	Description: Esse plugin é um plugin de teste
+	Version: 1.0
+	Author: Daniel Ramos
+	Author uri: https://www.webmaster.inf.br
+	License: GPLv2 or later
+*/
+
+function post_msg ($post_msg) {
+	$post_msg = "This message will be shwon in the beginning of each post";
+	$formatted_msg = "<h1> $post_msg </h1>";
+	$formatted_msg .= $post_msg;
+
+	return $formatted_msg;
+}
+
+add_filter( 'the_content', 'post_msg');
+
+?>
